b97ff2157cac98896afcbd33f2ecbc17e7174667
Federico Rossi <federico3@users.noreply.github.com>
2025-02-11 22:24:18 -0800

    Reactified the inputs

--- frontend/solarsystemclock/src/App.jsx
+++ frontend/solarsystemclock/src/App.jsx
@@ -1,29 +1,123 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useState } from "react";
 import './App.css'
 import DatePicker from './DatePicker'
 import Orrery from './Orrery'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  function dateSetter(){console.log("Date: "+document.getElementById('orrery-time').value)}
+  let _longitudes = []
+  for (let i=0; i<8; i++){
+    _longitudes.push(Math.PI/4*i);
+  }
+  // We will store the longitudes as the state.
+  const [longitudes, setLongitudes] = useState({
+    longitudes: _longitudes,
+  });
+  const [date, setDate] = useState({
+    date: Date.now(),
+  });
+  const [speed, setSpeed] = useState({
+    speed: 0
+  });
+
+  // When the date picker is updated, this is called
+  async function dateSetter(){
+    let utcTime_string = document.getElementById('orrery-time').value;
+    let utcTime_ms = Date.parse(utcTime_string);
+    console.log("Date: "+utcTime_string+" or in UNIX time "+utcTime_ms);
+    if (!isNaN(utcTime_ms)){
+      setDate({date: utcTime_string})
+      try{
+        const response = await fetch('/setDate/', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            unixDate_ms: utcTime_ms,
+          })
+        })
+        if (!response.ok) {
+          console.error(`Response status: ${response.status}`);
+        }
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+  }
+
+  // Help function to have a log speed slider
   function speedModifier(speed){return 10**speed};
-  function speedSetter(){
-    console.log("Speed: "+document.getElementById('orrery-speed').value)
-    console.log("ExpSpeed: "+speedModifier(document.getElementById('orrery-speed').value))
+  // When the speed is updated, this is called
+  async function speedSetter(){
+    let speed = document.getElementById('orrery-speed').value
+    let expspeed = speedModifier(speed)
+    console.log("Speed: "+speed)
+    console.log("ExpSpeed: "+expspeed)
+    setSpeed({speed: speed})
+    try{
+      const response = await fetch('/setSpeed/', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          speed: expspeed,
+        })
+      })
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  // This tries to fetch longitudes
+  async function longitudesGetter(){
+    console.log("Trying to get some longitudes!")
+    try{
+      const response = await fetch('/longitudes/')
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const lon_json = await response.json();
+      console.log(lon_json);
+      setLongitudes({longitudes: lon_json["longitudes"]})
+    } catch (error) {
+      console.error(error.message);
+      let _longitudes = []
+      let randoffset = Math.random()
+      for (let i=0; i<8; i++){
+        _longitudes.push(randoffset+Math.PI/4*i);
+      }
+      setLongitudes({longitudes: _longitudes})
+
+    }
   }
   
-  let longitudes = []
-  for (let i=0; i<8; i++){
-    longitudes.push(Math.PI/4*i);
-  }
 
+  // Register an effect to fetch longitudes every x seconds
+  useEffect(() => {
+    const intervalCall = setInterval(() => {
+      longitudesGetter();
+    }, 10000);
+    return () => {
+      // clean up
+      clearInterval(intervalCall);
+    };
+  }, []);
+
+  // Build the application
   return (
     <>
-    <DatePicker dateSetter={dateSetter} speedSetter={speedSetter} SpeedModifier={speedModifier}/>
-    <Orrery longitudes={longitudes}/>
+    <h1>Solar System Clock</h1>
+    <Orrery longitudes={longitudes.longitudes}/>
+
+    <DatePicker dateSetter={dateSetter} speedSetter={speedSetter} SpeedModifier={speedModifier} currentDate={date.date} currentSpeed={speed.speed}/>
     </>
   )
 }
--- frontend/solarsystemclock/src/DatePicker.jsx
+++ frontend/solarsystemclock/src/DatePicker.jsx
@@ -1,32 +1,45 @@
-import { useState } from 'react'
+import './DatePicker.css'
 
-function DatePicker({dateSetter,speedSetter,speedModifier}) {
-  const [count, setCount] = useState(0)
+function DatePicker({dateSetter,speedSetter,speedModifier,currentDate,currentSpeed}) {
 //   let currentSpeed = speedModifier(document.getElementById('orrery-speed').value)
   return (
     <>
       <div>
-        <label for="orrery-time">Choose a time:</label>
+        <label for="orrery-time">Date:</label>
         <input
         type="date"
         id="orrery-time"
         name="orrery-time"
         min="-3000-01-01T00:00"
         max="3000-01-01T00:00"
-        onInput={dateSetter}
+        value={currentDate}
+        // onInput={dateSetter}
+        onChange={dateSetter}
         />
       </div>
       <div>
         <label for="orrery-speed">Speed:</label>
         <input
-        type="range"
-        id="orrery-speed"
-        name="orrery-speed"
-        min="0"
-        max="10"
-        step="0.1"
-        onInput={speedSetter}
-       />
+            type="range"
+            id="orrery-speed"
+            name="orrery-speed"
+            min="0"
+            max="10"
+            step="0.1"
+            // onInput={speedSetter}
+            onChange={speedSetter}
+            value={currentSpeed}
+            list="speedvalues"
+        />
+
+        <datalist id="speedvalues">
+            <option value="0" label="1"></option>
+            <option value="2" label="100"></option>
+            <option value="4" label="10⁴"></option>
+            <option value="6" label="10⁶"></option>
+            <option value="8" label="10⁸"></option>
+            <option value="10" label="10¹⁰"></option>
+        </datalist>
       </div>
     </>
   )
